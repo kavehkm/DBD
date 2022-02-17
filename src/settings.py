6d7656605de24ea9a23bbfb2bbb7dc8833b8db91
kavehkm <mehrbaniankaveh@gmail.com>
2022-02-17 13:54:48 +0330

    create new module src/settings.py: to hold all project settings

---
+++ src/settings.py
@@ -0,0 +1,25 @@
+# standard
+import os
+import json
+
+
+# base directory
+BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
+
+
+# settings file
+SETTINGS_FILE = os.path.join(BASE_DIR, 'settings.json')
+
+
+# load settings
+try:
+    with open(SETTINGS_FILE, 'rt') as f:
+        SETTINGS = json.loads(f.read())
+except Exception as e:
+    # cannot load user settings
+    # log error and load default
+    SETTINGS = {}
+
+
+# databases
+DATABASES = SETTINGS.get('databases', [])
