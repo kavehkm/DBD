3f0ec2b626baed7d921d23f7844256b7cca719cd
kavehkm <mehrbaniankaveh@gmail.com>
2022-02-18 01:28:46 -0500

    update src/console.py: 1) add print 2) add error 3) some improvements in databases and tables

--- src/console.py
+++ src/console.py
@@ -1,17 +1,48 @@
 # rich
 from rich.tree import Tree
+from rich.panel import Panel
+from rich.columns import Columns
 from rich.console import Console
 
 
 console = Console()
 
 
+print = console.print
+
+
+def error(text):
+    print('[red bold]{}'.format(text))
+
+
 def menu(items):
     tree = Tree('')
     for item in items:
         tree.add(item)
-    console.print(tree)
+    print(tree)
 
 
 def widget_title(widget):
-    console.rule('[bold red]{}'.format(widget.NAME))
+    console.rule('[bold red]{}'.format(widget.NAME), style='[white]')
+
+
+def databases(databases):
+    columns = Columns()
+    for i, db in enumerate(databases):
+        panel = Panel(
+            title=str(i),
+            renderable='[bold red]{}[/bold red]\n[yellow]{}'.format(db.DBMS, db.name)
+        )
+        columns.add_renderable(panel)
+    print(columns)
+
+
+def tables(tables):
+    columns = Columns()
+    for i, table in enumerate(tables):
+        panel = Panel(
+            title=str(i),
+            renderable='[yellow]{}'.format(table)
+        )
+        columns.add_renderable(panel)
+    print(columns)
